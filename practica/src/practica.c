05713ed3af41787160b08916df1399abfd7d37af
jpcassagne <jpcassagne93@gmail.com>
2016-08-24 06:14:44 -0300

    Practica video

--- practica/src/practica.c
+++ practica/src/practica.c
@@ -2,6 +2,19 @@
 #include <stdlib.h>
 
 int main(void) {
-	printf("!!!Hola Mundo!!!");
+
+	printf("!!!Hola Mundo!!!\n");
+
+	int i;
+	char texto[256];
+
+	for (i = 0; i <= 15; ++i) {
+		printf("El numero de iteracion es: %d\n", i);
+
+	}
+	printf("Escribi algo:");
+	fgets(texto,256,stdin);
+	printf("El texto ingresado es: %s",texto);
+
 	return EXIT_SUCCESS;
 }
